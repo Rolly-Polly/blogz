e1bfce9d08348aa4e096bb4de8946ec82248adf0
Rosangela Hockman <rosangelarichel@hotmail.com>
2018-01-08 21:07:50 -0600

    Created user signup

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:ror1ch3l@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:123456@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
@@ -45,6 +45,56 @@ def id():
     post_id = int(request.args.get('id'))
     blog = Blog.query.get(post_id)
     return render_template('single_blog.html', blog=blog)    
+
+
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120))
+    password = db.Column(db.String(12))
+
+    def __init__(self,username, password):
+        self.username = username
+        self.password = password
+
+@app.route('/signup', methods = ['GET'])
+def signup_request():
+    return render_template('signup.html')
+
+
+@app.route('/signup', methods=['POST'])
+def signup(): 
+    username= request.form['username']
+    password= request.form['password']
+    verify_password= request.form['verify_password']
+    user_error= ""
+    pass_error= ""
+    
+    if username == "":
+        user_error = "Enter username"
+
+    elif len(username) < 3:
+        user_error = "Username is too short"
+    
+    if password == "":
+        pass_error = "Please enter Password"
+
+    elif verify_password != password:
+        pass_error = "Password does not match"
+    
+    if user_error == "" and pass_error == "":
+
+        user_signup= User(username, password)
+        db.session.add(user_signup)
+        db.session.commit()
+        
+        return redirect('/')
+
+    else:
+        return render_template('signup.html', user_error=user_error, pass_error=pass_error, verify_password=pass_error)
+
+
+    
 
 
   
