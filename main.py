c3be33dab0793fd3aa5ed4f1d5cfe12c2bb894da
Rosangela Hockman <rosangelarichel@hotmail.com>
2018-01-14 18:58:41 -0600

    Completed Blogz

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(120))
     password = db.Column(db.String(12))
-    blogs = db.relationship("Blog", backref="user")
+    blogs = db.relationship("Blog", backref="owner")
 
     def __init__(self,username, password):
         self.username = username
@@ -35,14 +35,14 @@ class User(db.Model):
 @app.before_request
 def require_login():
     print(request.endpoint)
-    allowed_routes = ['login', 'blogs', 'index', 'signup']
+    allowed_routes = ['login', 'login_request', 'blogs', 'index', 'signup', 'signup_request']
     if request.endpoint not in allowed_routes and 'user_id' not in session:
         return redirect('/login')
 
 @app.route('/', methods=['GET'])
 def index():
-    blogs = Blog.query.all()
-    return render_template('blogs.html', blogs=blogs)
+    users = User.query.all()
+    return render_template('index.html', users=users)
 
 
 @app.route('/add_blog', methods=['GET'])
@@ -54,18 +54,30 @@ def validate_form():
     if request.form["title"] == "" or request.form["body"] == "":       
         return render_template('add_blog.html', error_message="Please add a post")
     else:
+        user_id = session['user_id']
+        owner = User.query.get(user_id)
         title = request.form['title']
         body = request.form['body']
-        blog = Blog(title, body)
+        blog = Blog(title, body, owner)
         db.session.add(blog)
         db.session.commit()
         return redirect('/')
 
 @app.route('/blog', methods=['GET'])
-def id():
-    post_id = int(request.args.get('id'))
-    blog = Blog.query.get(post_id)
-    return render_template('single_blog.html', blog=blog)    
+def id(): 
+    post_id = request.args.get('id') 
+    user = request.args.get('user')
+    
+    if post_id is not None:
+        blog = Blog.query.get(post_id)
+        return render_template('single_blog.html', blog=blog)    
+
+    if user is not None:
+        user = User.query.get(user)
+        return render_template('blogs.html', blogs=user.blogs)
+
+    blogs = Blog.query.all()
+    return render_template('blogs.html', blogs=blogs)
 
 
 @app.route('/signup', methods = ['GET'])
@@ -99,10 +111,9 @@ def signup():
         db.session.add(user_signup)
         db.session.commit()
         
-        resp = make_response(redirect('/add_blog'))
-        resp.set_cookie('user_id', user_signup.id)
-
-        return resp
+        session['user_id'] = user_signup.id
+      
+        return redirect('/add_blog')
 
     else:
         return render_template('signup.html', user_error=user_error, pass_error=pass_error, verify_password=pass_error)
@@ -133,6 +144,7 @@ def login():
 def logout():
     del session['user_id']
     return redirect('/')
+
 
 
    
